7845a3aaadbdad5e38ae82805ba10aa30571265e
claudiart <claudia.reut@yahoo.com>
2021-03-18 10:06:04 +0100

    checkedCategories

--- backend/public/addPlace.js
+++ backend/public/addPlace.js
@@ -79,6 +79,42 @@ const submitPlace = (e) => {
 
     let checkedCategories = [];
     if ($("#friends").is(':checked')) {checkedCategories.push("friends")}
+    if ($("#meeting").is(':checked')) {checkedCategories.push("meeting")}
+    if ($("#business").is(':checked')) {checkedCategories.push("business")}
+    if ($("#date").is(':checked')) {checkedCategories.push("date")}
+    if ($("#family").is(':checked')) {checkedCategories.push("family")}
+    if ($("#eat").is(':checked')) {checkedCategories.push("eat")}
+    if ($("#breakfast").is(':checked')) {checkedCategories.push("breakfast")}
+    if ($("#brunch").is(':checked')) {checkedCategories.push("brunch")}
+    if ($("#lunch").is(':checked')) {checkedCategories.push("lunch")}
+    if ($("#dinner").is(':checked')) {checkedCategories.push("dinner")}
+    if ($("#latesnack").is(':checked')) {checkedCategories.push("latesnack")}
+    if ($("#aroundhere").is(':checked')) {checkedCategories.push("aroundhere")}
+    if ($("#fromthesea").is(':checked')) {checkedCategories.push("fromthesea")}
+    if ($("#fireland").is(':checked')) {checkedCategories.push("fireland")}
+    if ($("#bites").is(':checked')) {checkedCategories.push("bites")}
+    if ($("#bistrofrancais").is(':checked')) {checkedCategories.push("bistrofrancais")}
+    if ($("#cucinaitaliana").is(':checked')) {checkedCategories.push("cucinaitaliana")}
+    if ($("#farawayasia").is(':checked')) {checkedCategories.push("farawayasia")}
+    if ($("#plantbased").is(':checked')) {checkedCategories.push("plantbased")}
+    if ($("#streetfood").is(':checked')) {checkedCategories.push("streetfood")}
+    if ($("#hummus").is(':checked')) {checkedCategories.push("hummus")}
+    if ($("#fusion").is(':checked')) {checkedCategories.push("fusion")}
+    if ($("#sweettooth").is(':checked')) {checkedCategories.push("sweettooth")}
+    if ($("#patisserie").is(':checked')) {checkedCategories.push("patisserie")}
+    if ($("#bakery").is(':checked')) {checkedCategories.push("bakery")}
+    if ($("#gelateria").is(':checked')) {checkedCategories.push("gelateria")}
+    if ($("#wine").is(':checked')) {checkedCategories.push("wine")}
+    if ($("#cocktail").is(':checked')) {checkedCategories.push("cocktail")}
+    if ($("#beer").is(':checked')) {checkedCategories.push("beer")}
+    if ($("#coffee").is(':checked')) {checkedCategories.push("coffee")}
+    if ($("#highend").is(':checked')) {checkedCategories.push("highend")}
+    if ($("#easygoing").is(':checked')) {checkedCategories.push("easygoing")}
+    if ($("#between").is(':checked')) {checkedCategories.push("between")}
+    if ($("#dollar").is(':checked')) {checkedCategories.push("dollar")}
+    if ($("#ddollar").is(':checked')) {checkedCategories.push("ddollar")}
+    if ($("#dddollar").is(':checked')) {checkedCategories.push("dddollar")}
+
 
     let placeData = {
         name: name,
