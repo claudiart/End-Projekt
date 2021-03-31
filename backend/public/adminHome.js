2aa0c44ac98c3c36128483ee2cd0fcbb0bb849d4
Hristina Dakic <dakc@dakc@xxxlgroup.com>
2021-03-31 19:27:33 +0200

    fix edit place

--- backend/public/adminHome.js
+++ backend/public/adminHome.js
@@ -1,61 +1,70 @@
-$('#addPlace').on('click', () => window.location.href = "/admin/add");
-
+$("#addPlace").on("click", () => (window.location.href = "/admin/add"));
 
 fetch(`/places`, {
-    method: 'GET',
-    headers: { 'content-type': 'application/json; charset=UTF-8' },
-}).then(res => res.json())
-    .then(data => {
-        renderPlaces(data);
-    });
+  method: "GET",
+  headers: { "content-type": "application/json; charset=UTF-8" },
+})
+  .then((res) => res.json())
+  .then((data) => {
+    renderPlaces(data);
+  });
 
 const renderPlace = (place) => {
+  const liquids = place.liquids.length > 0 ? place.liquids : "";
+  const kitchen = place.kitchen.length > 0 ? place.kitchen : "";
 
-const liquids = place.liquids.length > 0 ? place.liquids : "";
-const kitchen = place.kitchen.length > 0 ? place.kitchen : "";
-
-    return (
-        `
+  return `
         <div class="col-md-6 col-lg-3 mb-3">
             <div class="content col-12 rounded shadow text-dark pt-3 pb-3">
                 <p class="text-dark font-weight-bold">${place.name}</p>
-                <p class="mt-3 mb-0">${place.address.streetAddress + ", " + place.address.postalCode + " " + place.address.city}</p>
+                <p class="mt-3 mb-0">${
+                  place.address.streetAddress +
+                  ", " +
+                  place.address.postalCode +
+                  " " +
+                  place.address.city
+                }</p>
                 <a href="${place.website}">${place.website}</a>
                 <p class="mb-0">${liquids}</p>
                 <p class="mb-0">${kitchen}</p>
-                <button class="btn btn-lg btn-block mt-3 mb-3" onclick='handleEdit("${place.id}")'>edit</button>
-                <button class="btn btn-lg btn-block mt-3 mb-3" onclick='handleDelete("${place.id}")'>delete</button>
+                <button class="btn btn-lg btn-block mt-3 mb-3" onclick='handleEdit("${
+                  place.id
+                }")'>edit</button>
+                <button class="btn btn-lg btn-block mt-3 mb-3" onclick='handleDelete("${
+                  place.id
+                }")'>delete</button>
             </div>
         </div>
-        `
-    )
-}
+        `;
+};
 
 const handleDelete = (placeId) => {
-    // http request with DELETE method
-    fetch(`/places/${placeId}`, {
-        method: "DELETE",
-        headers: { "content-type": "application/json; charset=UTF-8" },
-    }).then(response => alert("Place will be deleted")).then(location.reload())
-        .catch((error) => {
-            console.error("there was an error: ", error);
-        });
+  // http request with DELETE method
+  fetch(`/places/${placeId}`, {
+    method: "DELETE",
+    headers: { "content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => alert("Place will be deleted"))
+    .then(location.reload())
+    .catch((error) => {
+      console.error("there was an error: ", error);
+    });
 };
 
 const handleEdit = (placeId) => {
-    top.location.href = "/admin/edit/"+placeId;
-//     // http request with PUT method
-//     fetch(`/places/${placeName}`, {
-//         method: "PUT",
-//         headers: { "content-type": "application/json; charset=UTF-8" },
-//     }).then(console.log("I am here"))
-//         .catch((error) => {
-//             console.error("there was an error: ", error);
-//         });
+  top.location.href = "/admin/edit/" + placeId;
+  //     // http request with PUT method
+  //     fetch(`/places/${placeName}`, {
+  //         method: "PUT",
+  //         headers: { "content-type": "application/json; charset=UTF-8" },
+  //     }).then(console.log("I am here"))
+  //         .catch((error) => {
+  //             console.error("there was an error: ", error);
+  //         });
 };
 
 const renderPlaces = (data) => {
-    for (var place in data) {
-        $('#places').append(renderPlace(data[place]));
-    }
-}
+  for (var place in data) {
+    $("#places").append(renderPlace(data[place]));
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
-const { nanoid } = require('nanoid');
-const bcrypt = require('bcrypt');
+const { nanoid } = require("nanoid");
+const bcrypt = require("bcrypt");
 
-app.set("view engine", "hbs"); // hbs dateien statt html 
+app.set("view engine", "hbs"); // hbs dateien statt html
 const viewsPath = path.join(__dirname, "views"); // __dirname und views zusammenfassen als String
-app.set("views", viewsPath); // views in ViewsPath 
-app.use(express.static(path.join(__dirname, 'public'))); // 
+app.set("views", viewsPath); // views in ViewsPath
+app.use(express.static(path.join(__dirname, "public"))); //
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true })); // Middleware
 app.use(bodyParser.json());
 
@@ -19,252 +19,262 @@ const PORT = 3000;
 app.listen(PORT, () => console.log("Server Start at the Port " + PORT));
 
 //render register on start page (change this later to login)
-app.get('/', (req, res) => {
-	res.render('login')
-})
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
-app.get('/register', (req, res) => {
-	res.render('register')
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
+app.get("/user", (req, res) => {
+  res.render("../../frontend/index");
+});
 
-app.get('/user', (req, res) => {
-	res.render('../../frontend/index')
-})
+app.get("/admin", (req, res) => {
+  res.render("adminHome");
+});
 
-app.get('/admin', (req, res) => {
-	res.render('adminHome');
-})
+app.get("/admin/add", (req, res) => {
+  res.render("addPlace");
+});
 
-app.get('/admin/add', (req, res) => {
-		res.render('addPlace');
-})
-
-app.get('/admin/edit/:id', (req, res) => {
-	var placeId = req.params.id;
-	var data = fs.readFileSync('./places.json');
-	var places = JSON.parse(data);
-	//var ort = places.find( element => element.id == placeId );
-	for (let i in places ) {
-		if ( places[i].id == placeId ) {
-			var ort = places[i];
-		}
-	}
-	res.render('editPlace',{placeId:placeId, ortName:ort.name, ort:ort});
-})
+//Endpoint for editing place by id
+app.get("/admin/edit/:id", (req, res) => {
+  var placeId = req.params.id;
+  var data = fs.readFileSync("./places.json");
+  var places = JSON.parse(data);
+  //var ort = places.find( element => element.id == placeId );
+  for (let i in places) {
+    if (places[i].id == placeId) {
+      var place = places[i];
+    }
+  }
+  res.render("editPlace", {
+    place,
+  });
+});
 
 //Endpoint for getting all places
-app.get('/places', getAllPlaces);
+app.get("/places", getAllPlaces);
 
 function getAllPlaces(req, res) {
-	var data = fs.readFileSync('./places.json');
-	var places = JSON.parse(data);
-	if (places) {
-		res.json(places);
-	} else {
-		res.send("No places found");
-	}
+  var data = fs.readFileSync("./places.json");
+  var places = JSON.parse(data);
+  if (places) {
+    res.json(places);
+  } else {
+    res.send("No places found");
+  }
 }
 
 //Endpoint for getting a single place by name
-app.get('/places/:place', searchPlace);
+app.get("/places/:place", searchPlace);
 
 function searchPlace(req, res) {
-	var name = req.params.place.toLowerCase();
-	var data = fs.readFileSync('./places.json');
-	var places = JSON.parse(data);
-	if (places[name]) {
-		res.json(places[name]);
-	} else {
-		res.send("Place not found");
-	}
+  var name = req.params.place.toLowerCase();
+  var data = fs.readFileSync("./places.json");
+  var places = JSON.parse(data);
+  if (places[name]) {
+    res.json(places[name]);
+  } else {
+    res.send("Place not found");
+  }
 }
 
 //Endpoint for adding a place
-app.post('/places/add', addPlace);
+app.post("/places/add", addPlace);
 
 function addPlace(req, res) {
-	var newPlace = req.body;
-	if (newPlace.name) {
-		fs.readFile('places.json', function (err, data) {
-			var places = JSON.parse(data);
-			places[newPlace.name] = { id: nanoid(5), name: newPlace.name, address: newPlace.address, website: newPlace.website, kitchen: newPlace.kitchen, liquids: newPlace.liquids, categories: newPlace.categories };
-			fs.writeFile('places.json', JSON.stringify(places), (err) => {
-				if (err) {
-					throw err
-				}
-				console.log(newPlace.name + " has been added"); //if success then console.log this sentence
-			});
-		});
-		res.json(newPlace); //send response to client with newUser information
-	} else {
-		res.send('Failed to add place'); // if username, email and pass are NOT truthy (are missing) send response to client with this sentence
-		throw new Error('Failed to add place'); //and throw new error in the backend
-	}
+  var newPlace = req.body;
+  if (newPlace.name) {
+    fs.readFile("places.json", function (err, data) {
+      var places = JSON.parse(data);
+      places[newPlace.name] = {
+        id: nanoid(5),
+        name: newPlace.name,
+        address: newPlace.address,
+        website: newPlace.website,
+        kitchen: newPlace.kitchen,
+        liquids: newPlace.liquids,
+        categories: newPlace.categories,
+      };
+      fs.writeFile("places.json", JSON.stringify(places), (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log(newPlace.name + " has been added"); //if success then console.log this sentence
+      });
+    });
+    res.json(newPlace); //send response to client with newUser information
+  } else {
+    res.send("Failed to add place"); // if username, email and pass are NOT truthy (are missing) send response to client with this sentence
+    throw new Error("Failed to add place"); //and throw new error in the backend
+  }
 }
 
-//Endpoint for editing a place
-app.put('/places/:name', editPlace);
+//Endpoint for editing a place by id
+app.put("/places/:id", editPlace);
 
 function editPlace(req, res) {
-    var placeName = req.params.name;
-	console.log("'" + placeName + "'");
-	var updatedPlaceData = req.body;
-	console.log(updatedPlaceData);
-    if (placeName) {
-        fs.readFile('places.json', function (err, data) {
-            var places = JSON.parse(data);
-            //suche place mit dem namen placeName // gib das Objekt aus dem Array und überschreibe die Daten 
-			// var placeToUpdate = places[placeName];
-			//send this data to editPlace (redirect)???
-			
-			//update object with new values
-			places[placeName].name = updatedPlaceData.name; 
-			places[placeName].address = updatedPlaceData.address; 
-			places[placeName].website = updatedPlaceData.website;
-			places[placeName].kitchen = updatedPlaceData.kitchen;
-			places[placeName].liquids = updatedPlaceData.liquid;
-			places[placeName].categories = updatedPlaceData.categories;
-			
-			//befülle das Formular mit den daten
+  var placeId = req.params.id;
+  var updatedPlaceData = req.body;
+  console.log(updatedPlaceData);
+  if (placeId) {
+    fs.readFile("places.json", function (err, data) {
+      var places = JSON.parse(data);
+      //suche place mit dem namen placeName // gib das Objekt aus dem Array und überschreibe die Daten
+      // var placeToUpdate = places[placeName];
+      //send this data to editPlace (redirect)???
 
+      //update object with new values
+      places[placeId].name = updatedPlaceData.name;
+      places[placeId].address = updatedPlaceData.address;
+      places[placeId].website = updatedPlaceData.website;
+      places[placeId].kitchen = updatedPlaceData.kitchen;
+      places[placeId].liquids = updatedPlaceData.liquids;
+      places[placeId].categories = updatedPlaceData.categories;
 
-			//on save überschreibe place mit dem namen placeName mit neuen daten
-            fs.writeFile('places.json', JSON.stringify(places), (err) => {
-                if (err) {
-                    throw err
-                }
-            });
-            console.log(placeName + " has been edited");
-        });
-        res.json(placeName); 
-    } else {
-        res.send('Failed to edit place'); 
-        throw new Error('Failed to edit place'); 
-    }
+      //befülle das Formular mit den daten
 
+      //on save überschreibe place mit dem namen placeId mit neuen daten
+      fs.writeFile("places.json", JSON.stringify(places), (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+      console.log("Place with id" + placeId + " has been edited");
+    });
+    res.json(placeId);
+  } else {
+    res.send("Failed to edit place");
+    throw new Error("Failed to edit place");
+  }
 }
 
 //Endpoint for deleting a place
-app.delete('/places/:id', deletePlace);
+app.delete("/places/:id", deletePlace);
 
 function deletePlace(req, res) {
-    var placeId = req.params.id;
+  var placeId = req.params.id;
 
-    if (placeId) {
-        fs.readFile('places.json', function (err, data) {
-            var places = JSON.parse(data);
-            //delete places[placeName];
-			//places = places.filter( element => element.id != placeId );
-			for ( let i in places ) {
-				if ( places[i].id == placeId ) {
-					delete places [i]; break;
-				}
-			}
-            fs.writeFile('places.json', JSON.stringify(places), (err) => {
-                if (err) {
-                    throw err
-                }
-            });
-            console.log(placeId + " has been deleted");
-        });
-        res.json(placeId); 
-    } else {
-        res.send('Failed to delete place'); 
-        throw new Error('Failed to delete place'); 
-    }
-
+  if (placeId) {
+    fs.readFile("places.json", function (err, data) {
+      var places = JSON.parse(data);
+      //delete places[placeName];
+      //places = places.filter( element => element.id != placeId );
+      for (let i in places) {
+        if (places[i].id == placeId) {
+          delete places[i];
+          break;
+        }
+      }
+      fs.writeFile("places.json", JSON.stringify(places), (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+      console.log(placeId + " has been deleted");
+    });
+    res.json(placeId);
+  } else {
+    res.send("Failed to delete place");
+    throw new Error("Failed to delete place");
+  }
 }
-
-
 
 //Endpoint for user registration
 
 const registerUser = (req, res) => {
-	var newUser = req.body; //get user object from the request body and save it to newUser
-	if (newUser.username && newUser.email && newUser.pass) { //if username, email and pass are truthy ...
+  var newUser = req.body; //get user object from the request body and save it to newUser
+  if (newUser.username && newUser.email && newUser.pass) {
+    //if username, email and pass are truthy ...
 
-		fs.readFile('users.json', async (err, data) => { // ...read existing users data from users.js..
-			var users = JSON.parse(data); // ...parse that data to a JS object and save it to users...
+    fs.readFile("users.json", async (err, data) => {
+      // ...read existing users data from users.js..
+      var users = JSON.parse(data); // ...parse that data to a JS object and save it to users...
 
-			//check if user exists, if yes set userIsNew to false
-			let userIsNew = true;
+      //check if user exists, if yes set userIsNew to false
+      let userIsNew = true;
 
-			users.forEach(item => {
-				if (item.email == newUser.email) {
-					userIsNew = false;
-				}
-			})
+      users.forEach((item) => {
+        if (item.email == newUser.email) {
+          userIsNew = false;
+        }
+      });
 
-			if (userIsNew) {
-				newUser.admin = false; //... all users are false at the beginning...
-				newUser.id = nanoid(5);
-				await bcrypt.hash(newUser.pass, 8).then(hash => {
-					if (hash) {
-						newUser.pass = hash;
-					} else {
-						//todo smth if hash not valid
-					}
-				}).catch(err => console.log(err))
+      if (userIsNew) {
+        newUser.admin = false; //... all users are false at the beginning...
+        newUser.id = nanoid(5);
+        await bcrypt
+          .hash(newUser.pass, 8)
+          .then((hash) => {
+            if (hash) {
+              newUser.pass = hash;
+            } else {
+              //todo smth if hash not valid
+            }
+          })
+          .catch((err) => console.log(err));
 
-				users.push(newUser);//...push newUser to users array...
+        users.push(newUser); //...push newUser to users array...
 
-				fs.writeFile('users.json', JSON.stringify(users), (err) => { //stringify users (onverts a JavaScript value to a JSON string) and write it to users.json file
-					if (err) { //if there is an error throw error
-						throw err
-					}
-					console.log(newUser.username + " has been added"); //if success then console.log this sentence
-				});
-				newUser.ok = true; // wurde angelegt
-				console.log(newUser)
-				res.json(newUser); //send response to client with newUser information
-			} else {
-				// res.status(404);
-				// res.send('User already exists'); 
-				// throw new Error('User already exists');
-				res.json({ok:false});
-			};
-		});
-	
-	} else {
-		res.send('Failed to add user - missing data'); // if username, email and pass are NOT truthy (are missing) send response to client with this sentence
-		throw new Error('Failed to add user - missing data'); //and throw new error in the backend
-	}
-}
-app.post('/user', registerUser);
+        fs.writeFile("users.json", JSON.stringify(users), (err) => {
+          //stringify users (onverts a JavaScript value to a JSON string) and write it to users.json file
+          if (err) {
+            //if there is an error throw error
+            throw err;
+          }
+          console.log(newUser.username + " has been added"); //if success then console.log this sentence
+        });
+        newUser.ok = true; // wurde angelegt
+        console.log(newUser);
+        res.json(newUser); //send response to client with newUser information
+      } else {
+        // res.status(404);
+        // res.send('User already exists');
+        // throw new Error('User already exists');
+        res.json({ ok: false });
+      }
+    });
+  } else {
+    res.send("Failed to add user - missing data"); // if username, email and pass are NOT truthy (are missing) send response to client with this sentence
+    throw new Error("Failed to add user - missing data"); //and throw new error in the backend
+  }
+};
+app.post("/user", registerUser);
 
 //Endpoint for user login
-app.post('/login', loginUser);
+app.post("/login", loginUser);
 
 function loginUser(req, res) {
-	var userData = req.body;
-	if (userData.email && userData.pass) {
-		fs.readFile('users.json', function (err, data) {
-			var users = JSON.parse(data);
+  var userData = req.body;
+  if (userData.email && userData.pass) {
+    fs.readFile("users.json", function (err, data) {
+      var users = JSON.parse(data);
 
-			//find user by email	
-			const findUserByEmail = (email) => {
-				return users.find(user => email === user.email);
-			}
+      //find user by email
+      const findUserByEmail = (email) => {
+        return users.find((user) => email === user.email);
+      };
 
-			let foundUser = findUserByEmail(userData.email);
-	console.log(foundUser);
+      let foundUser = findUserByEmail(userData.email);
+      console.log(foundUser);
 
-			if (foundUser) { 
-				// bcrypt.compare(userData.pass, foundUser.pass).then(function(result) {
-				// 	res.json(JSON.stringify(result)); 
-				// 	// result true or false
-				bcrypt.compare(userData.pass, foundUser.pass).then(
-				res.json(JSON.stringify(foundUser)));
-
-			} else {
-				console.log("didn't find user");
-				throw new Error('didnt find user in database');
-			}
-		});
-	} else {
-		console.log("failed to log in");
-		throw new Error('no valid data found in request');
-	}
+      if (foundUser) {
+        // bcrypt.compare(userData.pass, foundUser.pass).then(function(result) {
+        // 	res.json(JSON.stringify(result));
+        // 	// result true or false
+        bcrypt
+          .compare(userData.pass, foundUser.pass)
+          .then(res.json(JSON.stringify(foundUser)));
+      } else {
+        console.log("didn't find user");
+        throw new Error("didnt find user in database");
+      }
+    });
+  } else {
+    console.log("failed to log in");
+    throw new Error("no valid data found in request");
+  }
 }
-
