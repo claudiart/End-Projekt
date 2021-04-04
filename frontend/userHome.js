890612a8581cbe9e3ad6d6b2aa495fe651771c22
Hristina Dakic <dakc@dakc@xxxlgroup.com>
2021-04-04 16:58:35 +0200

    filter add conditions

--- frontend/userHome.js
+++ frontend/userHome.js
@@ -1,16 +1,17 @@
 //fetch all places from backend
 
-const fetchPlaces = () =>
+var ignoreFilter = true;
+
+const fetchPlaces = () => {
   fetch(`/places`, {
     method: "GET",
     headers: { "content-type": "application/json; charset=UTF-8" },
-  });
-
-fetchPlaces()
-  .then((res) => res.json())
-  .then((data) => {
-    renderPlaces(data);
-  });
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      renderPlaces(data);
+    });
+};
 
 const renderPlace = (place) => {
   const liquids = place.liquids.length > 0 ? place.liquids : "";
@@ -35,35 +36,79 @@ const renderPlace = (place) => {
           `;
 };
 
+const isVisiblePlace = (place) => {
+  // console.log("isVisiblePlace: " + ignoreFilter);
+  // if (ignoreFilter) return true;
+  // console.log(place);
+  var isPlaceVisible = false;
+
+  const allKitchens = ["fireland"];
+
+  allKitchens.forEach((kitchen) => {
+    if (place.kitchen.includes(kitchen)) {
+      isPlaceVisible = true;
+    }
+  });
+
+  const allLiquids = ["cocktail"];
+
+  allLiquids.forEach((liquid) => {
+    if (place.liquids.includes(liquid)) {
+      isPlaceVisible = true;
+    }
+  });
+
+  const allCategories = ["dddollar"];
+
+  allCategories.forEach((category) => {
+    if (place.categories.includes(category)) {
+      isPlaceVisible = true;
+    }
+  });
+  // console.log("isVisiblePlace: RETURN FALSE");
+  console.log(isPlaceVisible);
+  return isPlaceVisible;
+};
+
 const renderPlaces = (data) => {
+  // console.log("RenderPlaces: " + ignoreFilter);
+  // console.log("RenderPlaces: " + data);
   for (var place in data) {
-    $("#places").append(renderPlace(data[place]));
+    // console.log("RenderPlaces: " + place);
+    if (isVisiblePlace(data[place])) {
+      // console.log(place + " is visible");
+      $("#places").append(renderPlace(data[place]));
+    } else {
+      // console.log(place + " is not visible");
+    }
   }
 };
 
 //find filtered places
-const filterPlaces = (e) => {
-  e.preventDefault();
-  console.log(e.target.id);
-  console.log(e.target.className);
-  fetchPlaces()
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      let filteredPlaces = {};
-      for (let i in places) {
-        if ($("#cocktail").is(":checked")) {
-          // filteredPlaces = places[i];
-        }
-      }
-      renderPlaces(filteredPlaces);
-    });
-  // $("#-tBHZ").addClass("d-none");
-};
+// const filterPlaces = (e) => {
+//   e.preventDefault();
+//   console.log(e.target.id);
+//   console.log(e.target.className);
+//   fetchPlaces()
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//       let filteredPlaces = {};
+//       for (let i in places) {
+//         if ($("#cocktail").is(":checked")) {
+//           // filteredPlaces = places[i];
+//         }
+//       }
+//       renderPlaces(filteredPlaces);
+//     });
+//   // $("#-tBHZ").addClass("d-none");
+// };
 
 const resetFilter = () => {
+  ignoreFilter = true;
   location.reload();
 };
 
-$("#apply").on("click", filterPlaces);
+$("#apply").on("click", fetchPlaces);
 $("#reset").on("click", resetFilter);
+$("#showall").on("click", fetchPlaces);
