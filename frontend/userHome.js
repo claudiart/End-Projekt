1f2bd96516b97ef19233e60da3318f1d5db03158
Hristina Dakic <dakc@dakc@xxxlgroup.com>
2021-04-04 12:15:53 +0200

    filter places try 2

--- frontend/userHome.js
+++ frontend/userHome.js
@@ -1,8 +1,12 @@
-console.log("hi");
-fetch(`/places`, {
-  method: "GET",
-  headers: { "content-type": "application/json; charset=UTF-8" },
-})
+//fetch all places from backend
+
+const fetchPlaces = () =>
+  fetch(`/places`, {
+    method: "GET",
+    headers: { "content-type": "application/json; charset=UTF-8" },
+  });
+
+fetchPlaces()
   .then((res) => res.json())
   .then((data) => {
     renderPlaces(data);
@@ -13,7 +17,7 @@ const renderPlace = (place) => {
   const kitchen = place.kitchen.length > 0 ? place.kitchen : "";
 
   return `
-          <div class="col-md-6 col-lg-3 mb-3">
+          <div id="${place.id}" class="col-md-6 col-lg-3 mb-3 ">
               <div class="content col-12 rounded shadow text-dark pt-3 pb-3">
                   <p class="text-dark font-weight-bold">${place.name}</p>
                   <p class="mt-3 mb-0">${
@@ -36,3 +40,28 @@ const renderPlaces = (data) => {
     $("#places").append(renderPlace(data[place]));
   }
 };
+
+//find filtered places
+const filterPlaces = (e) => {
+  e.preventDefault();
+  fetchPlaces()
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let filteredPlaces = {};
+      for (let i in places) {
+        if ($("#cocktail").is(":checked")) {
+          // filteredPlaces = places[i];
+        }
+      }
+      renderPlaces(filteredPlaces);
+    });
+  // $("#-tBHZ").addClass("d-none");
+};
+
+const resetFilter = () => {
+  location.reload();
+};
+
+$("#apply").on("click", filterPlaces);
+$("#reset").on("click", resetFilter);
