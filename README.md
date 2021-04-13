1242505a8a46b6f75403539c98ec38c514b2b797
claudiart <claudia.reut@yahoo.com>
2021-04-13 21:08:09 +0200

    99,1%

---
+++ README.md
@@ -0,0 +1,7 @@
+full stack crud app 
+Rest API 
+
+und file basierte datenbanken (.json)
+
+verwendeten technologien: node.js, express.js 
+frontend: JS/jQuery/ hbs 
--- public/adminHome.js
+++ public/adminHome.js
@@ -89,12 +89,3 @@ const handleEdit = (placeId) => {
   top.location.href = `/admin/edit/${placeId}`;
 };
  
-
-// full stack crud app 
-// Rest API 
-
-// und file basierte datenbanken (.json)
-
-// verwendeten technologien: node.js, express.js 
-// frontend: JS/jQuery/ hbs
- 
